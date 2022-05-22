6f0660189dc90e502f0453d9ffa7b1f593a7b0f8
GerdaBoman <88060352+GerdaBoman@users.noreply.github.com>
2022-05-22 23:19:35 +0200

    data anotations for validation

--- HamsterWars/Shared/Models/Hamster.cs
+++ HamsterWars/Shared/Models/Hamster.cs
@@ -7,24 +7,26 @@ public class Hamster
     [Key]
     public int Id { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Hamsters name is required")]
+    [StringLength(50, MinimumLength = 1, ErrorMessage = "Hamsters Name cannot have less than 1 characters and more than 50 characters in length")]
     public string Name { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Hamsters age is required")]
     public int Age { get; set; }
 
     [Display(Name = "Favorite Food")]
-    [DisplayFormat(ConvertEmptyStringToNull = true, NullDisplayText = "N/A")]
+    [Required]
     public string FavFood { get; set; }
 
-    [DisplayFormat(ConvertEmptyStringToNull = true, NullDisplayText = "N/A")]
-    public string Loves { get; set; }
+
     [Required]
+    public string Loves { get; set; }
+
+    [Required(ErrorMessage = "Please select a picture")]
     public string ImgName { get; set; }
     public int Wins { get; set; }
     public int Losses { get; set; }
     public int Games { get; set; }
 
-   
 
 }
